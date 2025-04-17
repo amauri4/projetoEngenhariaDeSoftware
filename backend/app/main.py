98fb181f3e50999a51608e1212f58cfd128310ba
amauri4 <amaurilacerda2004@gmail.com>
2025-04-17 12:22:09 -0300

    refactor(backend): removendo importações descenessárias da main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,4 @@
 from database.base import engine, Base
-from models.HabitoBase import HabitoBase
-from models.CategoriasHabito import CategoriaHabito
-from models.RegistroDiario import RegistroDiario
-from database import session
-from models.Usuario import Usuario
-from datetime import datetime
-from repositories.UsuarioRepositories import UserRepository
 
 # Usado para criar tabela no bd (*ALTERAR FUTURAMENTE, POIS NÃO INCLUI MUDANÇAS NAS ESTRUTURA DAS TABELAS -> usar alembic*)
 def init_db():
